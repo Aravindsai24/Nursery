d7cd9a06888116149fe755c9c9416fcb1f275ef6
Pritam Das <mepritamdas1498@outlook.com>
2021-08-04 13:29:01 +0530

    ImageView Implemented using Glide in RecyclerView

--- app/src/main/java/com/example/nursery/MyAdapter.kt
+++ app/src/main/java/com/example/nursery/MyAdapter.kt
@@ -1,21 +1,25 @@
 package com.example.nursery
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class MyAdapter(private val plantList: ArrayList<Plant>) :
+class MyAdapter(private val plantList: ArrayList<Plant>, var context: Context?) :
     RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val pName: TextView = view.findViewById(R.id.PlantName)
         val availability: TextView = view.findViewById(R.id.avail)
         val pPrice: TextView = view.findViewById(R.id.cost)
-
+        val pImg: ImageView =view.findViewById(R.id.item_image_view)
     }
-
+  //var mContext = context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+       // mContext=parent.context
         val itemView =
             LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return MyViewHolder(itemView)
@@ -23,13 +27,16 @@ class MyAdapter(private val plantList: ArrayList<Plant>) :
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val plant: Plant = plantList[position]
+        //https://www.simplyfresh.co.in/wp-content/uploads/2019/11/3_Oregano.png
+       Glide.with(context).load(plant.pImg).into(holder.pImg)
         holder.pName.text = plant.pName
-        holder.pPrice.text = plant.pPrice
+        holder.pPrice.text = "₹"+plant.pPrice
         holder.availability.text = if (plant.availability == true) {
             "Yes"
         } else {
             "No"
         }
+       // holder.itemView.
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/nursery/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/nursery/ui/dashboard/DashboardFragment.kt
@@ -5,10 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nursery.MyAdapter
@@ -49,7 +46,7 @@ class DashboardFragment : Fragment() {
 
         plantArrayList = arrayListOf()
 
-        myAdapter = MyAdapter(plantArrayList)
+        myAdapter = MyAdapter(plantArrayList, activity?.applicationContext)
 
         recyclerView.adapter = myAdapter
 
